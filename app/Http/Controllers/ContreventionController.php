ab422f34d84a697e24e3c1f5b4eb7c8f78bed581
silasmas <silasjmas@gmail.com>
2024-11-23 15:09:39 +0100

    Update ContreventionController.php

--- app/Http/Controllers/ContreventionController.php
+++ app/Http/Controllers/ContreventionController.php
@@ -342,7 +342,7 @@ class ContreventionController extends Controller
             $ret = json_decode($payment);
 
 
-            return view('transaction_message', [
+            return view('pages.transaction_message', [
                 'status_code' => $code,
                 'message_content' => "Transaction annulée"
             ]);
